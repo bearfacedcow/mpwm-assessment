62548d620dbc26e421f12abd4d1a552965b91612
bearfacedcow <jord@bearfacedcow.com>
2019-10-21 20:10:56 -0400

    Initial Commit

---
+++ routes/l33t.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/:theWord", function(req, res, next) {
+  const l33tMap = [
+    { char: "a", mapTo: "4" },
+    { char: "e", mapTo: "3" },
+    { char: "i", mapTo: "1" },
+    { char: "o", mapTo: "0" },
+    { char: "s", mapTo: "5" },
+    { char: "t", mapTo: "7" }
+  ];
+
+  const original = req.params.theWord;
+  const charList = original.split("");
+  let result = "";
+
+  charList.forEach(c => {
+    const l33tChar = l33tMap
+      .filter(f => c.toLowerCase() === f.char)
+      .map(r => r.mapTo)
+      .reduce((out, ch) => (out = ch ? ch : out), c);
+    result += l33tChar;
+  });
+
+  res.json({ original, result });
+});
+
+module.exports = router;
---
+++ routes/rle.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/:theWord", function(req, res, next) {
+  const original = req.params.theWord;
+  const charList = original.split("");
+
+  let lastLetter = "";
+  let letterCount = 0;
+  let result = "";
+
+  charList.forEach(c => {
+    if (c === lastLetter) {
+      letterCount++;
+    } else {
+      result += letterCount ? `${lastLetter}${letterCount}` : "";
+      lastLetter = c;
+      letterCount = 1;
+    }
+  });
+
+  result += letterCount ? `${lastLetter}${letterCount}` : "";
+
+  res.json({ original, result });
+});
+
+module.exports = router;
